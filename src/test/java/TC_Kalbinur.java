1788069c80e9f0ea60f0438d895e4a269dcd6e5c
KalbinurK <kkalbinur@yahoo.com>
2020-11-01 16:10:46 -0500

    updated TC001

--- src/test/java/TC_Kalbinur.java
+++ src/test/java/TC_Kalbinur.java
@@ -3,6 +3,8 @@ public class TC_Kalbinur {
     public static void main(String[] args) {
 
         System.out.println("working on TC 001");
+
+        System.out.println("updated TC001");
     }
 
 }
