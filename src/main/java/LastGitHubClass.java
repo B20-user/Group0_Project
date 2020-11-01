95b84e0b7c78cc398a9f5c6c000cc49ee2d8e99a
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-11-01 16:26:54 -0500

    extra memory need to add in my brain.

--- src/main/java/LastGitHubClass.java
+++ src/main/java/LastGitHubClass.java
@@ -1,2 +1,5 @@
 public class LastGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Yes, learned a lot about GIT AND GitHub that i need to add memory");
+    }
 }
