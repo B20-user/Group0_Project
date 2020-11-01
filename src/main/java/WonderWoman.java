45762b01146e4fb464d542d34424eccca7fa0052
bugunerli <gunerlibulent@gmail.com>
2020-11-01 13:27:07 -0700

    Updated last changes

--- src/main/java/WonderWoman.java
+++ src/main/java/WonderWoman.java
@@ -2,5 +2,6 @@ public class WonderWoman {
 
     public static void main(String[] args) {
         System.out.println("WonderWomen is wondering herself.");
+        // WonderWomen will get offer from Tesla.
     }
 }
