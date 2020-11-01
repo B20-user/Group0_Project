da6463785e4293edd734c4de55dd663dbae0fc06
erfanjava <evan.abula7@gmail.com>
2020-11-01 16:27:06 -0500

    Last github commit

--- src/test/java/LastGithubClass.java
+++ src/test/java/LastGithubClass.java
@@ -2,5 +2,7 @@ public class LastGithubClass {
     public static void main(String[] args) {
 
         System.out.println("I hope i wont forget this stuff");
+
+        System.out.println("Fist one got merge without approve somehow");
     }
 }
